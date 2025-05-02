9a6892edfba54ba4ceb6c6149233122c05595491
iamraselmolla <raselmolla6336@gmail.com>
2025-05-02 23:59:35 +0600

    comment

--- src/helpers/FileUploadHelper.ts
+++ src/helpers/FileUploadHelper.ts
@@ -4,39 +4,38 @@ import * as fs from 'fs';
 import { ICloudinaryResponse, IUploadFile } from '../interfaces/file';
 import config from '../config';
 
+// Configure Cloudinary
 cloudinary.config({
-    cloud_name: config.cloudinary.cloudName,
-    api_key: config.cloudinary.apiKey,
-    api_secret: config.cloudinary.apiSecret
+  cloud_name: config.cloudinary.cloudName,
+  api_key: config.cloudinary.apiKey,
+  api_secret: config.cloudinary.apiSecret
 });
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
 });
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 const uploadToCloudinary = async (file: IUploadFile): Promise<ICloudinaryResponse | undefined> => {
-    return new Promise((resolve, reject) => {
-        cloudinary.uploader.upload(file.path,
-            (error: Error, result: ICloudinaryResponse) => {
-                fs.unlinkSync(file.path);
-                if (error) {
-                    reject(error)
-                }
-                else {
-                    resolve(result)
-                }
-            })
-    })
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(file.path, (error: Error, result: ICloudinaryResponse) => {
+      fs.unlinkSync(file.path);
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
 };
 
 export const FileUploadHelper = {
-    uploadToCloudinary,
-    upload
-}
+  uploadToCloudinary,
+  upload
+};
